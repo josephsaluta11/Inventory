02c82c13a4b19613a7bb011f6cbe06d5887103ab
josephsaluta11 <91950106+josephsaluta11@users.noreply.github.com>
2021-12-09 21:53:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/inventory/Home.java
@@ -0,0 +1,87 @@
+package com.example.inventory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Home extends AppCompatActivity {
+
+    private Button bCreateNewInv, bManageInv;
+
+    private TextInputLayout useremail_outlinedbox;
+    private TextInputEditText user_email;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private MaterialButton logout_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bCreateNewInv = findViewById(R.id.bCreateNewInv);
+        bManageInv = findViewById(R.id.bManageInv);
+
+        useremail_outlinedbox = findViewById(R.id.useremail_outlinedbox);
+        user_email = findViewById(R.id.user_email);
+        logout_btn = findViewById(R.id.logout_btn);
+        useremail_outlinedbox.setEnabled(false);
+        user_email.setEnabled( false );
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        user_email.setText(currentUser.getEmail());
+
+        logout_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                startActivity(new Intent(Home.this, Login.class));
+                finish();
+            }
+        });
+
+        bCreateNewInv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { createNewInventory(); }
+        });
+
+        bManageInv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { manageInventory(); }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() ==null){
+            startActivity(new Intent(Home.this,Login.class));
+        }
+    }
+
+
+    public void createNewInventory() {
+        Intent intent = new Intent(this, NewInventory.class);
+        startActivity(intent);
+    }
+
+    public void manageInventory() {
+        Intent intent = new Intent(this, ManageActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/inventory/Signup.java
@@ -0,0 +1,159 @@
+package com.example.inventory;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Signup extends AppCompatActivity {
+
+    private TextInputEditText userEmail, userPassword, cuserPassword;
+    private MaterialButton register_btn;
+    private TextView login_tv;
+    private FirebaseAuth mAuth;
+    private String TAG;
+    private ProgressBar register_progress;
+    String userID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        userEmail = findViewById(R.id.email_edit_text);
+        userPassword = findViewById(R.id.password_edit_text);
+        cuserPassword = findViewById(R.id.cpassword_edit_text);
+        register_btn = findViewById(R.id.register_btn);
+        register_progress = findViewById(R.id.register_progress);
+        login_tv = findViewById(R.id.login_tv);
+        login_tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Signup.this, Login.class));
+                finish();
+            }
+        });
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    RegisterUserAccount();
+                }
+
+        });
+
+    }
+
+
+
+    private void RegisterUserAccount(){
+
+        final String email = userEmail.getText().toString();
+        String password = userPassword.getText().toString();
+        String cpassword = cuserPassword.getText().toString();
+
+        if (email.isEmpty()) {
+            userEmail.setError("Email is empty");
+            userEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            userEmail.setError("Not a valid email address");
+            userEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            userPassword.setError("Password is empty");
+            userPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            userPassword.setError("Must be more than 6 characters");
+            userPassword.requestFocus();
+            return;
+        }
+
+        if (!password.equals(cpassword)) {
+            userPassword.setError("Password did not match");
+            userPassword.requestFocus();
+            return;
+        }
+
+        register_btn.setVisibility(View.GONE);
+        register_progress.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+
+                            final User user = new User(
+                                    email
+                            );
+
+                            FirebaseUser usernameinfirebase = mAuth.getCurrentUser();
+                            String UserID = usernameinfirebase.getEmail();
+
+                            String resultemail = UserID.replace(".","");
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(resultemail).child("UserDetails")
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    register_progress.setVisibility(View.GONE);
+                                    if (task.isSuccessful()) {
+
+                                        Toast.makeText(Signup.this, "Account Created Successfully", Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(Signup.this, Home.class));
+
+                                    } else {
+
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(Signup.this,"Failed to Create an Account" + task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            register_btn.setVisibility(View.VISIBLE);
+                            register_progress.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null){
+            finish();
+            startActivity(new Intent(Signup.this, Home.class));
+        }
+    }
+}
